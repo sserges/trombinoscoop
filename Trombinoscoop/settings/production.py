9284fceadfa33a7587ab9f538b648a6569c37263
jack <jackthebestripper@yahoo.com>
2018-09-29 06:32:41 +0000

    Production settings file added

--- Trombinoscoop/settings/production.py
+++ Trombinoscoop/settings/production.py
@@ -13,7 +13,7 @@ DEBUG = False
 SECRET_KEY = os.environ.get('SECRET_KEY', '+xz#p9=p*ahiz4l0pnp(lyhb^6gxe^7i^$=#$uj&(bs(v6cg=_')
 
 ALLOWED_HOSTS = [
-    'trombinoscoop.herokuapp.com',  
+    'trombinoscoop-2.herokuapp.com',  
 ]
 
 MIDDLEWARE += ['whitenoise.middleware.WhiteNoiseMiddleware',]
